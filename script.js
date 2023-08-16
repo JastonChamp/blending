b2da4bf637d9f6f64b740dc0a7bacc3fa703d980
JastonChamp <142294191+JastonChamp@users.noreply.github.com>
2023-08-16 14:12:26 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const slots = document.querySelectorAll('.slot');
+const wheel = document.querySelector('.wheel');
 const cvcWords = [
   'hop', 'nut', 'bed', 'cat', 'dog', 'pen', 'run', 'bug', 'fox', 'hat',
   'jam', 'net', 'map', 'pig', 'tub', 'cup', 'van', 'wax', 'win', 'box',
@@ -6,23 +6,25 @@ const cvcWords = [
   'web', 'yak', 'zip', 'fog', 'leg', 'mop', 'sad', 'bed', /* Add more words */
 ];
 
+// Insert the CVC words into the slots
+const slots = document.querySelectorAll('.slot');
+slots.forEach((slot, index) => {
+  const wordIndex = index % cvcWords.length;
+  slot.textContent = cvcWords[wordIndex];
+});
+
 let spinning = false;
+let currentDegree = 0;
 
 document.getElementById('spinButton').addEventListener('click', () => {
   if (!spinning) {
     spinning = true;
-    const randomIndex = Math.floor(Math.random() * cvcWords.length);
-    slots.forEach((slot, index) => {
-      const newIndex = (index + randomIndex) % cvcWords.length;
-      slot.style.transition = 'none';
-      setTimeout(() => {
-        slot.style.transition = 'transform 1s ease-in-out';
-        slot.style.transform = `rotateX(${newIndex * (360 / cvcWords.length)}deg)`;
-        slot.textContent = cvcWords[newIndex];
-        if (index === slots.length - 1) {
-          spinning = false;
-        }
-      }, 0);
-    });
+    const randomDegree = Math.floor(Math.random() * 360) + 360 * 5; // 5 full spins
+    const totalDegree = currentDegree + randomDegree;
+    wheel.style.transform = `rotateX(${totalDegree}deg)`;
+    currentDegree = totalDegree % 360;
+    setTimeout(() => {
+      spinning = false;
+    }, 1000); // 1s is the duration of the rotation transition
   }
 });
