470639ccad9b52d0f2e04c2630b814b6a0ca0b5a
JastonChamp <142294191+JastonChamp@users.noreply.github.com>
2023-08-16 15:07:18 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const cvcWords = [
   'hop', 'nut', 'bed', 'cat', 'dog', 'pen', 'run', 'bug', 'fox', 'hat',
   'jam', 'net', 'map', 'pig', 'tub', 'cup', 'van', 'wax', 'win', 'box',
   'fan', 'jug', 'kid', 'lid', 'mat', 'pig', 'rat', 'sun', 'tap', 'vet',
-  'web', 'yak', 'zip', 'fog', 'leg', 'mop', 'sad', 'bed', /* Add more words */
+  'web', 'yak', 'zip', 'fog', 'leg', 'mop', 'sad', 'bed'
 ];
 
 // Insert the CVC words into the slots
@@ -22,7 +22,7 @@ document.getElementById('spinButton').addEventListener('click', () => {
     const slotsCount = slots.length; // Number of slots
     const randomSlot = Math.floor(Math.random() * slotsCount);
     const fullSpins = 5; // Number of full spins
-    const totalDegree = (360 * fullSpins) + (randomSlot * 360 / slotsCount);
+    const totalDegree = (360 * fullSpins) + (randomSlot * (360 / slotsCount));
     const finalDegree = currentDegree + totalDegree;
     wheel.style.transform = `rotateX(${finalDegree}deg)`;
     currentDegree = finalDegree % 360;
@@ -30,6 +30,4 @@ document.getElementById('spinButton').addEventListener('click', () => {
       spinning = false;
     }, 1000); // 1s is the duration of the rotation transition
   }
-});
-
 });
