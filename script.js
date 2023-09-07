6452c8efc78114c3d666985a2956e2f6bdcebab9
JastonChamp <142294191+JastonChamp@users.noreply.github.com>
2023-09-07 16:22:37 +0800

    Update script.js

--- script.js
+++ script.js
@@ -22,22 +22,27 @@ let currentDegree = 0;
 
 
 
+
+
+let currentDegree = 0;
+
 document.getElementById('spinButton').addEventListener('click', () => {
   if (!spinning) {
     spinning = true;
-    const slotsCount = cvcWords.length; // Number of slots
+    const slotsCount = cvcWords.length;
     const randomSlot = Math.floor(Math.random() * slotsCount);
-    const fullSpins = 5; // Number of full spins
+    const fullSpins = 5;
     const totalDegree = (360 * fullSpins) + (randomSlot * (360 / slotsCount));
     const finalDegree = currentDegree + totalDegree;
-    const adjustment = finalDegree % (360 / slotsCount); // Calculate the adjustment needed to center the slot
+    const adjustment = finalDegree % 360; // Calculate the adjustment needed to center the slot
     wheel.style.transform = `rotateY(${finalDegree - adjustment}deg)`; // Apply the adjustment
     currentDegree = (finalDegree - adjustment) % 360;
     setTimeout(() => {
       spinning = false;
-    }, 1000); // 1s is the duration of the rotation transition
+    }, 1000);
   }
 });
+
 
 
 
