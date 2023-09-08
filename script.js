a8bee7dbec67d6d8ed3a2b0f039d6276fa574ebf
JastonChamp <142294191+JastonChamp@users.noreply.github.com>
2023-09-08 10:47:38 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,24 @@
 const wheel = document.querySelector('.wheel');
 const cvcWords = [
   'hop', 'nut', 'bed', 'cat', 'dog', 'pen', 'run', 'bug', 'fox', 'hat',
-  'jam', 'net', 'map', 'pig', 'tub', 'cup', 'van', 'wax', 'win', 'box',
-  'fan', 'jug', 'kid', 'lid', 'mat', 'pig', 'rat', 'sun', 'tap', 'vet',
-  'web', 'yak', 'zip', 'fog', 'leg', 'mop', 'sad', 'bed'
+  'jam', 'net', 'map', 'pig', 'tub', 'cup', 'van', 'wax', 'win', 'box'
 ];
 
 cvcWords.forEach((word, index) => {
   const slot = document.createElement('div');
   slot.className = 'slot';
   slot.textContent = word;
-  slot.style.zIndex = 0;  // Initialize z-index to 0 for all slots
   wheel.appendChild(slot);
 });
 
-let spinning = false;
-let currentSlot = 0;
-
 document.getElementById('spinButton').addEventListener('click', () => {
-  if (!spinning) {
-    spinning = true;
-    const slots = document.querySelectorAll('.slot');
-    slots[currentSlot].style.zIndex = 0;  // Reset the z-index of the current slot
-
-    // Randomly select a new slot
-    const randomSlot = Math.floor(Math.random() * cvcWords.length);
-    slots[randomSlot].style.zIndex = 1;  // Bring the new slot to the front
-
-    currentSlot = randomSlot;  // Update the current slot
-
-    setTimeout(() => {
-      spinning = false;
-    }, 1000);
-  }
+  const slots = document.querySelectorAll('.slot');
+  const randomIndex = Math.floor(Math.random() * slots.length);
+  slots.forEach((slot, index) => {
+    if (index === randomIndex) {
+      slot.style.backgroundColor = 'yellow';
+    } else {
+      slot.style.backgroundColor = 'white';
+    }
+  });
 });
